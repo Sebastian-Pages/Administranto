347a4e650008d52a8fe87ea8cbcb7213851f7dc4
Sebastian-Pages <yves-sebastian.pages@telecom-sudparis.eu>
2021-11-09 21:41:13 +0100

    I removed files by accident

---
+++ web_app/script.js
@@ -0,0 +1,6 @@
+import { Sortable, Plugins } from '@shopify/draggable';
+
+const Classes = {
+    draggable: 'StackedListItem--isDraggable',
+    capacity: 'draggable-container-parent--capacity',
+};
