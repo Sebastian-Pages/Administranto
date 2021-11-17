fc3a3369fc84d227bab370504438615d8231bb0e
Sebastian_Pages <yves-sebastian.pages@etu.u-bordeaux.fr>
2021-11-17 15:02:49 +0100

    delete files on main

--- web_app/script.js
+++
@@ -1,6 +0,0 @@
-import { Sortable, Plugins } from '@shopify/draggable';
-
-const Classes = {
-    draggable: 'StackedListItem--isDraggable',
-    capacity: 'draggable-container-parent--capacity',
-};
