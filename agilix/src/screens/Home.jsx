f487385414b96f9bb12a06c31dd982b0d2863d3f
Sebastian-Pages <yves-sebastian.pages@telecom-sudparis.eu>
2021-12-13 22:18:56 +0100

    rename

--- agilix/src/screens/Home.jsx
+++ agilix/src/screens/Home.jsx
@@ -1,21 +1,20 @@
-import {db} from '../firebase/fbConfig'
-import {BrowserRouter, Route} from 'react-router-dom'
+import { db } from '../firebase/fbConfig'
+import { BrowserRouter, Route } from 'react-router-dom'
 import useBoards from '../hooks/useBoards'
 import useSprints from '../hooks/useSprints'
 
 import BoardList from '../components/BoardList'
 
-import Kanban from './Kanban'
+import Sprint from './Sprint'
 import RealKanban from './RealKanban'
 
-import {v4 as uuidv4} from 'uuid';
+import { v4 as uuidv4 } from 'uuid';
 
 
-const Home = ({logOut, userId, loginWithGoogle, name, isAnon}) => 
-{
+const Home = ({ logOut, userId, loginWithGoogle, name, isAnon }) => {
 
-    const boards = useBoards(userId)    
-    // const sprints = useSprints(boards)    
+    const boards = useBoards(userId)
+        // const sprints = useSprints(boards)    
 
     const addNewBoard = (e) => {
         e.preventDefault()
@@ -23,25 +22,25 @@ const Home = ({logOut, userId, loginWithGoogle, name, isAnon}) =>
 
         db.collection(`users/${userId}/boards`)
             .doc(uid)
-            .set({name: e.target.elements.boardName.value , endingDate: e.target.elements.endingProjectDate.value})
+            .set({ name: e.target.elements.boardName.value, endingDate: e.target.elements.endingProjectDate.value })
 
         e.target.elements.boardName.value = ''
         e.target.elements.endingProjectDate.value = ''
 
     }
 
-    const addSprint = (e,bid) => {
-        console.log("add sprint: ",bid)
+    const addSprint = (e, bid) => {
+        console.log("add sprint: ", bid)
 
         e.preventDefault()
         const uid = uuidv4()
 
         db.collection(`users/${userId}/boards/${bid}/sprints`)
             .doc(uid)
-            .set({name: e.target.elements.boardName.value , endingDate: e.target.elements.endingProjectDate.value})
-        
+            .set({ name: e.target.elements.boardName.value, endingDate: e.target.elements.endingProjectDate.value })
+
         /**** est Déplacer dans add Sprint ***********/
-        const columnOrder = {id: 'columnOrder', order: ['productBacklog']}
+        const columnOrder = { id: 'columnOrder', order: ['productBacklog'] }
 
         db.collection(`users/${userId}/boards/${bid}/sprints/${uid}/columns`)
             .doc('columnOrder')
@@ -53,7 +52,7 @@ const Home = ({logOut, userId, loginWithGoogle, name, isAnon}) =>
         db.collection(`users/${userId}/boards/${bid}/sprints/${uid}/columns`)
             .doc('productBacklog')
             .set(productBacklog)
-        /********************************/
+            /********************************/
 
     }
 
@@ -63,34 +62,50 @@ const Home = ({logOut, userId, loginWithGoogle, name, isAnon}) =>
             .delete()
     }
 
-    const addNewSprint = (e) => { 
-    }
+    const addNewSprint = (e) => {}
 
-    const gotoSprint= (e) => {
-
-    }
-
-    const viewSprint= (e) => {
+    const gotoSprint = (e) => {
 
     }
 
-    return boards !== null ? (
-         <BrowserRouter>
-                <Route exact path='/'>
-                    <BoardList deleteBoard={deleteBoard} logOut={logOut} boards={boards} addNewBoard={addNewBoard} name={name}/>
-                </Route>
+    const viewSprint = (e) => {
 
-                <Route path='/board/:boardId'>
-                    <Kanban logOut={logOut} boards={boards} userId={userId} addSprint={addSprint}/>
-                </Route>
+    }
 
-                <Route path='RealKanban/:boardId/:sprintId'>
-                    <RealKanban userId={userId} />
-                </Route>
+    return boards !== null ? ( <
+        BrowserRouter >
+        <
+        Route exact path = '/' >
+        <
+        BoardList deleteBoard = { deleteBoard }
+        logOut = { logOut }
+        boards = { boards }
+        addNewBoard = { addNewBoard }
+        name = { name }
+        /> <
+        /Route>
 
-            </BrowserRouter>
+        <
+        Route path = '/board/:boardId' >
+        <
+        Sprint logOut = { logOut }
+        boards = { boards }
+        userId = { userId }
+        addSprint = { addSprint }
+        /> <
+        /Route>
 
-    ) : <div className="spinner h-screen w-screen" />
+        <
+        Route path = 'RealKanban/:boardId/:sprintId' >
+        <
+        RealKanban userId = { userId }
+        /> <
+        /Route>
+
+        <
+        /BrowserRouter>
+
+    ) : < div className = "spinner h-screen w-screen" / >
 }
 
 export default Home
